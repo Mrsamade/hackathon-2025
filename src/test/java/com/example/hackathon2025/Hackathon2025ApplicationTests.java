f316bceadf05e3f1dc0f82e07aa6cd6db0f83121
Mrsamade <133805559+Mrsamade@users.noreply.github.com>
2025-11-22 13:45:06 +0000

    first commit

---
+++ src/test/java/com/example/hackathon2025/Hackathon2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackathon2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
